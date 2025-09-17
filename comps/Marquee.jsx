a11b488a6aeb25a096e9603fd490d6783f107d95
AqeelAbbasr12 <aqeelabbasr12@gmail.com>
2025-09-17 14:39:57 +0500

    Updated point 5

--- comps/Marquee.jsx
+++ comps/Marquee.jsx
@@ -11,7 +11,9 @@ const Marquee = ({ products }) => {
       </h1>
 
       <section className=" mt-10 relative h-52 sm:h-96  w-full  overflow-hidden">
-        <div className={`flex  justify-center flex-row-reverse`}>
+        <div
+          className={`flex  justify-center flex-row-reverse ${styles.scrollWrapper}`}
+        >
           {products.map((product) => (
             <Products gap={"mr-5"} key={product._id} products={product} />
           ))}
